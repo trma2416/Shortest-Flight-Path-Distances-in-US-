544efb1a124df3802e2632c40b15010f3c213aa8
Tristan Martinez <tristandmartinez99@gmail.com>
2025-04-30 14:53:06 -0600

    Updated test suite

--- tests/tests_final.cpp
+++ tests/tests_final.cpp
@@ -1,18 +1,24 @@
-//from graph test cpp file
 #include <cmath>
-#include <ctime>
-#include <fstream>
 #include <gtest/gtest.h>
-#include <iostream>
-#include <math.h>
-#include <string>
-
 #include "../code/final.h"
 using namespace std;
+//in our tests, we will chech that our edge distances differences between
+//our cmputed weigths and the actual weights are within this EPSILON
+constexpr float EPSILON = 1e-4f;
+
+//this tests our initialization of our nodes 
+//and member functions to ensure they are working properly
+TEST(NodeTest, initialization){
+    Node_p node("cityA");
+    //made up coordinates and distances
+    node.set_coord(40.0f, 20.0f);
+    node.set_d(100.0f);
+
+    EXPECT_EQ(node.get_name(), "cityA");
+    EXPECT_NEAR(node.get_lat(), 40.0f, EPSILON);
+    EXPECT_NEAR(node.get_long(), 20.0f, EPSILON);
+    EXPECT_NEAR(node.get_dist(), 100.0f, EPSILON);
+    
 
 
-//forward declarations
-Graph* make_graph();
-
-
-TEST_F()
+}

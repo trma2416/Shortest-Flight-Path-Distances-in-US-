5162cd84a40ef21c939ca432cfaf8f1e5b87f223
Tristan Martinez <tristandmartinez99@gmail.com>
2025-05-05 22:27:49 -0600

    I think Im acutally finished now

--- app/main.cpp
+++ app/main.cpp
@@ -13,12 +13,13 @@ int main(){
     
     string start;
     string finish;
-
+    
+    
     while(start != "Q"){
 
         cout << "Please Enter a Starting City, or enter Q to quit (must be a US state capital)" << endl;
         
-        cin>>start;
+        getline(cin,start);
         if(start == "Q"){
             break;
         }
@@ -28,24 +29,25 @@ int main(){
         }
         if(in_graph){
             cout << "Valid Starting Location, Please enter a Destination city" << endl;
-            cin>>finish;
+            getline(cin, finish);
         }
         bool fin_in_graph = false;
         if(G->get_node_by_name(finish) != nullptr){
             fin_in_graph = true;
             
         }
-        if(fin_in_graph){
+        if(fin_in_graph && finish != start){
             cout << "Valid Destination" << endl;
             
             vector<Node_p*> path = G->dijkstra(start, finish);
             
             cout << "Distance traveled " << path[1]->get_d() << endl;
+            cout << "\n" << endl;
             cout << "Places visited along way ";
             for(auto i: path){
                 cout << i->get_name() << ", ";
             }
-            cout << endl;
+            cout << "\n" << endl;
         }
 
         
